a1a373baab4beeee9ebbe502a886f9dce11801a0
sachow12 <salomaovasques2@gmail.com>
2024-11-21 13:56:09 -0300

    First Commit

---
+++ src/test/java/hospital/db/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital.db.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
